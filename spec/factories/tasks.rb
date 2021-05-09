71ba7a11e5bab8f459240a42f4ba081da4a3f606
Kaori Toyonaga <kaori_toyonaga@yahoo.co.jp>
2021-05-09 13:46:44 +0000

    tested sorting

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,6 +1,10 @@
 FactoryBot.define do
   factory :task do
-    title { 'title' }
-    detail { 'detail' }    
+    title { 'title1' }
+    detail { 'detail1' }
+  end
+  factory :second_task, class: Task do
+    title { 'title2' }
+    detail { 'detail2' }
   end
 end
--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -1,10 +1,5 @@
 require 'rails_helper'
 RSpec.describe 'タスク管理機能', type: :system do
-  let!(:task) { FactoryBot.create(:task, title: 'task') }
-  before do
-    # 「一覧画面に遷移した場合」や「タスクが作成日時の降順に並んでいる場合」など、contextが実行されるタイミングで、before内のコードが実行される
-    visit tasks_path
-  end
 
   describe '新規作成機能' do
     context 'タスクを新規作成した場合' do
@@ -28,34 +23,35 @@ RSpec.describe 'タスク管理機能', type: :system do
     context '一覧画面に遷移した場合' do
       it '作成済みのタスク一覧が表示される' do
         # テストで使用するためのタスクを作成
-        # task = FactoryBot.create(:task, title: 'task')
+        task = FactoryBot.create(:task, title: 'title1')
         # タスク一覧ページに遷移
         visit tasks_path
         # visitした（遷移した）page（タスク一覧ページ）に「task」という文字列が
         # have_contentされているか（含まれているか）ということをexpectする（確認・期待する）
-        expect(page).to have_content 'task'
+        expect(page).to have_content 'title1'
         # expectの結果が true ならテスト成功、false なら失敗として結果が出力される
       end
     end
+  end
 
     context 'タスクが作成日時の降順に並んでいる場合' do
       it '新しいタスクが一番上に表示される' do
+        task = FactoryBot.create(:task, title: 'title1')
+        second_task = FactoryBot.create(:task, title: 'title2')
         visit tasks_path
         task_list = all('.task_row')
-        # ここに実装する
+        expect(task_list[0]).to have_content 'title2'
+        expect(task_list[1]).to have_content 'title1'
       end
     end
-
-  end
 
   describe '詳細表示機能' do
      context '任意のタスク詳細画面に遷移した場合' do
        it '該当タスクの内容が表示される' do
-         task = FactoryBot.create(:task, title: 'task')
+         task = FactoryBot.create(:task, title: 'title1')
          visit task_path(task.id)
          expect(page).to have_content '#:'
        end
      end
   end
-
 end
