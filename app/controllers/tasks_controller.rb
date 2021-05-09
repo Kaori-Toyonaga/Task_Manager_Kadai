22ab8eacc258305b4e18697f5039d02a70a83466
Kaori Toyonaga <kaori_toyonaga@yahoo.co.jp>
2021-05-09 07:55:08 +0000

    added New&Edit function

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,39 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: [:show]
+  before_action :set_task, only: [:show, :edit, :update]
 
   def index
     @tasks = Task.all
   end
 
   def show
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if params[:back]
+      render :new
+    else
+      if @task.save
+        redirect_to tasks_path, notice: "タスクを作成しました。"
+      else
+        render :new
+      end
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @task.update(task_params)
+       redirect_to tasks_path, notice: "更新しました。"
+    else
+      render :edit
+    end
   end
 
   private
