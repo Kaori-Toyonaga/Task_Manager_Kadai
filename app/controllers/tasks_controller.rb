8279f8b68a7eb95779de6f1f808b932b2672a8ba
Kaori Toyonaga <kaori_toyonaga@yahoo.co.jp>
2021-05-09 07:43:16 +0000

    created Index&Show views

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,20 @@
+class TasksController < ApplicationController
+  before_action :set_task, only: [:show]
+
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+  end
+
+  private
+  def task_params
+    params.require(:task).permit(:title, :detail)
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+end
