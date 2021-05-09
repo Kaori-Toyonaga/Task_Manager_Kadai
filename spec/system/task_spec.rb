4489c8e9ca7d77666511c75585a8bf0be49f4013
Kaori Toyonaga <kaori_toyonaga@yahoo.co.jp>
2021-05-09 12:28:28 +0000

    sorted the list

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 RSpec.describe 'タスク管理機能', type: :system do
+  let!(:task) { FactoryBot.create(:task, title: 'task') }
+  before do
+    # 「一覧画面に遷移した場合」や「タスクが作成日時の降順に並んでいる場合」など、contextが実行されるタイミングで、before内のコードが実行される
+    visit tasks_path
+  end
+
   describe '新規作成機能' do
     context 'タスクを新規作成した場合' do
       it '作成したタスクが表示される' do
@@ -22,7 +28,7 @@ RSpec.describe 'タスク管理機能', type: :system do
     context '一覧画面に遷移した場合' do
       it '作成済みのタスク一覧が表示される' do
         # テストで使用するためのタスクを作成
-        task = FactoryBot.create(:task, title: 'task')
+        # task = FactoryBot.create(:task, title: 'task')
         # タスク一覧ページに遷移
         visit tasks_path
         # visitした（遷移した）page（タスク一覧ページ）に「task」という文字列が
@@ -31,6 +37,15 @@ RSpec.describe 'タスク管理機能', type: :system do
         # expectの結果が true ならテスト成功、false なら失敗として結果が出力される
       end
     end
+
+    context 'タスクが作成日時の降順に並んでいる場合' do
+      it '新しいタスクが一番上に表示される' do
+        visit tasks_path
+        task_list = all('.task_row')
+        # ここに実装する
+      end
+    end
+
   end
 
   describe '詳細表示機能' do
@@ -42,4 +57,5 @@ RSpec.describe 'タスク管理機能', type: :system do
        end
      end
   end
+
 end
