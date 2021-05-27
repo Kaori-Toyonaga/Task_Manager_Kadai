330a01357b7e19df606c801c2cbdffd055920d43
Kaori Toyonaga <kaori_toyonaga@yahoo.co.jp>
2021-05-27 14:16:06 +0000

    add index

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_27_114947) do
+ActiveRecord::Schema.define(version: 2021_05_27_141410) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2021_05_27_114947) do
     t.integer "status"
     t.integer "priority"
     t.datetime "created_at"
+    t.index ["title"], name: "index_tasks_on_title"
   end
 
 end
