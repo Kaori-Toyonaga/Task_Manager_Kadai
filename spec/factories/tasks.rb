43d49ffdf087bdb8d5f78567c939f06261461cf0
Kaori Toyonaga <kaori_toyonaga@yahoo.co.jp>
2021-05-27 14:04:15 +0000

    test searching(title & status) fanctions

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -9,27 +9,27 @@ FactoryBot.define do
   #   detail { 'detail2' }
   # end
 
-  factory :task do
-    expired_at { DateTime }
-    detail { 'detail1' }
-    title { 'title1' }
-  end
-  factory :second_task, class: Task do
-    expired_at { DateTime }
-    detail { 'detail2' }
-    title { 'title2' }
-  end
-
   # factory :task do
-  #   title { 'title1' }
-  #   status { '未着手' }
+  #   expired_at { DateTime }
   #   detail { 'detail1' }
+  #   title { 'title1' }
   # end
   # factory :second_task, class: Task do
-  #   title { 'sample' }
-  #   status { '着手中' }
+  #   expired_at { DateTime }
   #   detail { 'detail2' }
+  #   title { 'title2' }
   # end
+
+  factory :task do
+    title { 'title1' }
+    status { '未着手' }
+    detail { 'detail1' }
+  end
+  factory :second_task, class: Task do
+    title { 'sample' }
+    status { '着手中' }
+    detail { 'detail2' }
+  end
 
   # factory :task do
   #   priority { '中' }
