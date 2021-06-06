d46f80a650632d29e26a859527442945528b5fa6
Kaori Toyonaga <kaori_toyonaga@yahoo.co.jp>
2021-06-06 19:10:08 +0000

    write spec test

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,30 @@
 FactoryBot.define do
+  # factory :user do
+  #   name { 'user' }
+  #   email { 'user02@user.com' }
+  #   password { '000000' }
+  #   password_confirmation { '000000' }
+  # end
+
   factory :user do
-    
+    name { 'テスト　太郎' }
+    email { 'test@test.com' }
+    password { '111111' }
+    password_confirmation { '000000' }
+  end
+
+  factory :user do
+    name { 'user' }
+    email { 'user@user.com'}
+    admin { true }
+    password { '000000' }
+    password_confirmation { '000000' }
+  end
+  factory :second_user, class: User do
+    name { 'user2' }
+    email { 'user2@user.com' }
+    admin { false }
+    password { '111111' }
+    password_confirmation { '111111' }
   end
 end
