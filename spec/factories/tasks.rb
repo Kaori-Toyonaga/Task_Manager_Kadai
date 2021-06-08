96f9166a38f54fd0fd79a820eb92898360c760b3
Kaori Toyonaga <kaori_toyonaga@yahoo.co.jp>
2021-06-08 15:52:47 +0000

    Rspec test writing check

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -3,42 +3,50 @@ FactoryBot.define do
   #   title { 'title1' }
   #   status { '未着手' }
   #   detail { 'detail1' }
+  # association :user
   # end
   # factory :second_task, class: Task do
   #   title { 'title2' }
   #   detail { 'detail2' }
+  #   association :user
   # end
 
   # factory :task do
   #   expired_at { DateTime }
   #   detail { 'detail1' }
   #   title { 'title1' }
+  #   association :user
   # end
   # factory :second_task, class: Task do
   #   expired_at { DateTime }
   #   detail { 'detail2' }
   #   title { 'title2' }
+  #   association :user
   # end
 
   factory :task do
     title { 'title1' }
     status { '未着手' }
     detail { 'detail1' }
+    association :user
   end
   factory :second_task, class: Task do
     title { 'sample' }
     status { '着手中' }
     detail { 'detail2' }
+    association :user
   end
 
   # factory :task do
   #   priority { '中' }
   #   detail { 'detail1' }
   #   title { 'title1' }
+  #   association :user
   # end
   # factory :second_task, class: Task do
   #   priority { '高' }
   #   detail { 'detail2' }
   #   title { 'title2' }
+  #   association :user
   # end
 end
